535137f158c6447e42e83170fa977258c12b3d9e
Mingding <hanmd82@gmail.com>
2015-05-28 00:28:49 +0800

    Update initial app configuration

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Sensifydemoapp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Singapore'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -30,5 +30,14 @@ module Sensifydemoapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_record.default_timezone = :local
+    config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.helper      false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
